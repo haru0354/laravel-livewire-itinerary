84b716da8d1f54b12fe1f0e8cb313bb8f693fc0b
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-04-04 13:53:50 +0900

    migrationでMemosテーブルの作成

--- database/migrations/2025_04_04_045050_create_memos_table.php
+++ database/migrations/2025_04_04_045050_create_memos_table.php
@@ -11,16 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('itineraries', function (Blueprint $table) {
+        Schema::create('memos', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('trip_id')->constrained()->onDelete('cascade');
-            $table->dateTime('date_and_time');
             $table->string('title', 30);
             $table->text('content', 200)->nullable();
-            $table->text('hide_content', 200)->nullable();
-            $table->string('image_url', 255)->nullable();
-            $table->string('image_alt', 255)->nullable();
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('itineraries');
+        Schema::dropIfExists('memos');
     }
 };
