ca719989d76a8e56f89121123ec10f13d24d78d3
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-04-05 14:44:05 +0900

    Dashboardのheaderのロゴのリンクをトップページに変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,8 +50,8 @@
             @endif
         </header>
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
-            <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
-                aa
+            <main>
+                トップページ
             </main>
         </div>
 
