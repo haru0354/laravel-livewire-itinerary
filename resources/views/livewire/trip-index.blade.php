ce9e258da95ba60dc9ce4ffcec1c8550b45d6c39
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-04-07 14:06:56 +0900

    button-linkコンポーネントの作成

--- resources/views/livewire/trip-index.blade.php
+++ resources/views/livewire/trip-index.blade.php
@@ -77,6 +77,7 @@
     <!-- UIテスト -->
     <x-form.input name="a">テスト </x-form.input>
     <x-form.textarea name="aa">テスト </x-form.textarea>
+    <x-ui.button-link :route="route('top')">テスト </x-ui.button-link>
     <x-ui.button size="normal">テスト </x-ui.button>
 
 
