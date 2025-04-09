e56d0f7191cf45602ac04d1dfd24436b9b8e353f
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-04-09 13:19:14 +0900

    メモの一覧表示部分のmarginを狭める

--- resources/views/livewire/trip-show.blade.php
+++ resources/views/livewire/trip-show.blade.php
@@ -7,7 +7,7 @@
         </div>
         <div class="flex flex-wrap justify-center">
             @foreach ($trip->memos as $memo)
-            <div class="relative flex-wrap w-full max-w-[480px] my-6 mx-4 p-6 shadow-lg hover:shadow-2xl transition-shadow rounded-lg border border-gray-300 bg-gray-50">
+            <div class="relative flex-wrap w-full max-w-[480px] my-3 mx-3 p-6 shadow-lg hover:shadow-2xl transition-shadow rounded-lg border border-gray-300 bg-gray-50">
                 <h3 class="pb-1 mt-2 mb-4 text-xl text-center font-bold border-b border-gray-700 border-dashed">{{ $memo->title }}</h3>
                 <p>{{ $memo->content }}</p>
                 <x-ui.edit-button wire="openEditMemoModal({{ $memo->id }})" />
