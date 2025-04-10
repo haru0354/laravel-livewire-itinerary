e81dbd745e69f4e28a0efbfa689679091998692a
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-04-10 13:23:57 +0900

    旅程の一覧を取得して表示まで作成

--- app/Livewire/TripItinerary.php
+++ app/Livewire/TripItinerary.php
@@ -2,10 +2,21 @@
 
 namespace App\Livewire;
 
+use App\Models\Itinerary;
 use Livewire\Component;
 
 class TripItinerary extends Component
 {
+    public $trip_id;
+    public $itineraries;
+
+    public function mount($trip_id)
+    {
+        $this->itineraries = Itinerary::where('trip_id', $trip_id)
+            ->orderBy('date_and_time', 'asc')
+            ->get();
+    }
+
     public function render()
     {
         return view('livewire.trip-itinerary');
--- resources/views/livewire/trip-itinerary.blade.php
+++ resources/views/livewire/trip-itinerary.blade.php
@@ -1,3 +1,16 @@
 <div>
-    trip-Itinerryのテスト
+    <div class="mb-16">
+        <div class="flex justify-between items-center">
+            <h3 class="w-full text-2xl font-semibold">旅程の一覧</h3>
+            <x-ui.button  color="blue">➕ 旅程の追加</x-ui.button>
+        </div>
+        @foreach ($itineraries as $itinerary)
+        <div class="relative flex flex-col my-6 p-6 border border-gray-300 rounded-lg shadow-lg hover:shadow-2xl transition-shadow bg-gray-50">
+            <p class="mb-3">⌚️ {{ $itinerary->date_and_time }}</p>
+            <h3 class="mb-2 text-xl font-bold">🗺 {{ $itinerary->title }}</h3>
+            <p class="mt-2">{{ $itinerary->content }}</p>
+            <p>{{ $itinerary->hide_content }}</p>
+        </div>
+        @endforeach
+    </div>
 </div>
